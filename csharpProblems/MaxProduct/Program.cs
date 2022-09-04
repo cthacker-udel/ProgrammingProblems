7c93f0a87ff32377c3c6519d837008c686d0017a
cthacker-udel <cthacker@udel.edu>
2022-09-04 15:37:11 -0400

    [main] Added CS projects

---
+++ csharpProblems/MaxProduct/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static int MaxProduct(int[] arr, int size)
+    {
+        List<int> sorted = arr.ToList();
+        sorted.Sort();
+        sorted.Reverse();
+        return sorted.Take(size).Aggregate((e1, e2) => e1 * e2);
+    }
+}
---
+++ csharpProblems/WordsToMarks/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int WordsToMarks(string str)
+    {
+        string lower = "abcdefghijklmnopqrstuvwxyz";
+        int sum = str.ToLower().ToCharArray().Select(e => lower.IndexOf(e) + 1).Sum();
+        return sum;
+    }
+}
